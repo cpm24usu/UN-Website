89c015388009faeaa47fb86f49c0a79e0e53fc93
cpm24usu <cpm24usu@uea.ac.uk>
2024-10-28 11:44:12 +0000

    Update index.js

--- C3 Project/Scripts/index.js
+++ C3 Project/Scripts/index.js
@@ -25,22 +25,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
             const logoDisplay = document.getElementById("logo");
             const horizLogo = document.createElement("img");
-            horizLogo.src = data.media[0].HorizontalLogo;
+            horizLogo.src = data.media.HorizontalLogo;
             logoDisplay.appendChild(horizLogo).setAttribute("id", "logoJS");
 
             const goal6 = document.getElementById("goal6");
             const imageElement = document.createElement("img");
-            imageElement.src = data.media[0].goal6;
+            imageElement.src = data.media.goal6;
             goal6.appendChild(imageElement).setAttribute("id", "imgLink1");
 
             const goal12 = document.getElementById("goal12");
             const imageElement2 = document.createElement("img");
-            imageElement2.src = data.media[0].goal12;
+            imageElement2.src = data.media.goal12;
             goal12.appendChild(imageElement2).setAttribute("id", "imgLink2");
 
             const goal13 = document.getElementById("goal13");
             const imageElement3 = document.createElement("img");
-            imageElement3.src = data.media[0].goal13;
+            imageElement3.src = data.media.goal13;
             goal13.appendChild(imageElement3).setAttribute("id", "imgLink3");
 
         })
