de82605c3948b9bd0db6d7352eec32b58e3c2d26
clowntown05 <cms24eau@uea.ac.uk>
2024-11-22 18:17:34 +0000

    added more to goal6 page, imported font to use for website

--- C3-Project/EJS/views/Scripts/goal6.js
+++ C3-Project/EJS/views/Scripts/goal6.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         // Main page content //
 
-        let goalContent = document.getElementById("goalContent");
+        let goalContent = document.getElementById("mainSection");
         const title = document.createElement("h1");
         title.textContent = responseData.goal6.content.title;
         goalContent.appendChild(title);
@@ -13,6 +13,48 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const goalOverview = document.createElement("p");
         goalOverview.textContent = responseData.goal6.content.overview;
         goalContent.appendChild(goalOverview);
+
+        const issue = document.createElement("h3");
+        issue.textContent = responseData.goal6.content.issue;
+        goalContent.appendChild(issue);
+
+        const issueInfo = document.createElement("p");
+        issueInfo.textContent = responseData.goal6.content.issueInfo;
+        goalContent.appendChild(issueInfo);
+
+        let targets = document.getElementById("goalTargets");
+        
+        
+        const targetsTitle = document.createElement("h2");
+        targetsTitle.textContent = responseData.goal6.content.targetsTitle;
+        targets.appendChild(targetsTitle);
+        
+        const scrollBox = document.createElement("article");
+        targets.appendChild(scrollBox);
+        
+        const targetsList = [7];
+        targetsList[0] = responseData.goal6.content.targets.target1;
+        targetsList[1] = responseData.goal6.content.targets.target2;
+        targetsList[2] = responseData.goal6.content.targets.target3;
+        targetsList[3] = responseData.goal6.content.targets.target4;
+        targetsList[4] = responseData.goal6.content.targets.target5;
+        targetsList[5] = responseData.goal6.content.targets.target6;
+        targetsList[6] = responseData.goal6.content.targets.target7;
+    
+        
+       // console.log(targetsList);
+        const tList = document.createElement("ol"); 
+        for (var i = 0; i < targetsList.length; i++) {
+            let listItem = document.createElement('li');
+            listItem.innerText = targetsList[i];
+            tList.appendChild(listItem);
+
+        }
+        scrollBox.appendChild(tList);        
+
+    
+
+
 
     })
 
