06c5bc80bfc63e9d51af8470f189476b85399e90
cpm24usu <cpm24usu@uea.ac.uk>
2024-12-03 13:01:33 +0000

    Fixed goal image large at bottom of page

--- C3-Project/EJS/views/Scripts/index.js
+++ C3-Project/EJS/views/Scripts/index.js
@@ -1,94 +1,92 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (document.querySelector('#homepage')){ //checks if the current page is the homepage using an id="homepage" in the opening html tag
-        fetch('../JSON/main.JSON')
-        .then(response => response.json())
-        .then(data => {
-            
-            // Main Paragraph - Intro to site
+    fetch('../JSON/main.JSON')
+    .then(response => response.json())
+    .then(data => {
+        
+        // Main Paragraph - Intro to site
 
-            const dataDisplay = document.getElementById("mainPara");
+        const dataDisplay = document.getElementById("mainPara");
 
-            const title = document.createElement("h1");
-            title.textContent = data.homepage.content.title;
+        const title = document.createElement("h1");
+        title.textContent = data.homepage.content.title;
 
-            const intro = document.createElement("p");
-            intro.textContent = data.homepage.content.intro;
-
-
-            const goal6header = document.createElement("h2");
-            const goal6para = document.createElement("p");
-            goal6header.textContent = data.homepage.content.goal6header;
-            goal6para.textContent = data.homepage.content.goal6;
-
-            const goal12header = document.createElement("h2");
-            const goal12para = document.createElement("p");
-            goal12header.textContent = data.homepage.content.goal12header;
-            goal12para.textContent = data.homepage.content.goal12;
-
-            const goal13header = document.createElement("h2");
-            const goal13para = document.createElement("p");
-            goal13header.textContent = data.homepage.content.goal13header;
-            goal13para.textContent = data.homepage.content.goal13;
-
-            const outro = document.createElement("p");
-            outro.textContent = data.homepage.content.outro;
-            outro.setAttribute("id", "outro");
+        const intro = document.createElement("p");
+        intro.textContent = data.homepage.content.intro;
 
 
-            dataDisplay.appendChild(title);
-            dataDisplay.appendChild(intro);
-            dataDisplay.appendChild(goal6header);
-            dataDisplay.appendChild(goal6para);
-            dataDisplay.appendChild(goal12header);
-            dataDisplay.appendChild(goal12para);
-            dataDisplay.appendChild(goal13header);
-            dataDisplay.appendChild(goal13para);
-            dataDisplay.appendChild(outro);
+        const goal6header = document.createElement("h2");
+        const goal6para = document.createElement("p");
+        goal6header.textContent = data.homepage.content.goal6header;
+        goal6para.textContent = data.homepage.content.goal6;
+
+        const goal12header = document.createElement("h2");
+        const goal12para = document.createElement("p");
+        goal12header.textContent = data.homepage.content.goal12header;
+        goal12para.textContent = data.homepage.content.goal12;
+
+        const goal13header = document.createElement("h2");
+        const goal13para = document.createElement("p");
+        goal13header.textContent = data.homepage.content.goal13header;
+        goal13para.textContent = data.homepage.content.goal13;
+
+        const outro = document.createElement("p");
+        outro.textContent = data.homepage.content.outro;
+        outro.setAttribute("id", "outro");
 
 
-            /*Loading test content into 2nd central element*/
+        dataDisplay.appendChild(title);
+        dataDisplay.appendChild(intro);
+        dataDisplay.appendChild(goal6header);
+        dataDisplay.appendChild(goal6para);
+        dataDisplay.appendChild(goal12header);
+        dataDisplay.appendChild(goal12para);
+        dataDisplay.appendChild(goal13header);
+        dataDisplay.appendChild(goal13para);
+        dataDisplay.appendChild(outro);
 
-            const testPara = document.getElementById("testPara");
 
-            const allGoals = document.createElement("img");
-            allGoals.src = data.homepage.media.allGoals;
-            allGoals.setAttribute("class", "goalImg");
-            allGoals.setAttribute("alt", "Image showing all 17 goals from the UN website");
+        /*Loading test content into 2nd central element*/
 
-            testPara.appendChild(allGoals);
+        const testPara = document.getElementById("testPara");
+
+        const allGoals = document.createElement("img");
+        allGoals.src = data.homepage.media.allGoals;
+        allGoals.setAttribute("class", "goalImg");
+        allGoals.setAttribute("alt", "Image showing all 17 goals from the UN website");
+
+        testPara.appendChild(allGoals);
 
 
-            /*Left images section via JSON*/
+        /*Left images section via JSON*/
 
-            const goalsImages = document.getElementById("goalImages");
+        const goalsImages = document.getElementById("goalImages");
 
-            const goalsVideo = document.createElement("video");
-            goalsVideo.src = data.homepage.media.mp4;
-            goalsVideo.setAttribute("class", "goalVideo");
-            goalsVideo.setAttribute("controls", "controls");
-            goalsVideo.setAttribute("alt","Short video from the UN website showing all 17 SDGs");
+        const goalsVideo = document.createElement("video");
+        goalsVideo.src = data.homepage.media.mp4;
+        goalsVideo.setAttribute("class", "goalVideo");
+        goalsVideo.setAttribute("controls", "controls");
+        goalsVideo.setAttribute("alt","Short video from the UN website showing all 17 SDGs");
 
-            const goalImg2 = document.createElement("img");
-            goalImg2.src = data.homepage.media.goal6;
-            goalImg2.setAttribute("class", "goalImg");
-            goalImg2.setAttribute("alt", "Image for goal 6 from the UN website");
+        const goalImg2 = document.createElement("img");
+        goalImg2.src = data.homepage.media.goal6;
+        goalImg2.setAttribute("class", "goalImg");
+        goalImg2.setAttribute("alt", "Image for goal 6 from the UN website");
 
-            const goalImg3 = document.createElement("img");
-            goalImg3.src = data.homepage.media.goal12;
-            goalImg3.setAttribute("class", "goalImg");
-            goalImg3.setAttribute("alt", "Image for goal 12 from the UN website");
+        const goalImg3 = document.createElement("img");
+        goalImg3.src = data.homepage.media.goal12;
+        goalImg3.setAttribute("class", "goalImg");
+        goalImg3.setAttribute("alt", "Image for goal 12 from the UN website");
 
-            const goalImg4 = document.createElement("img");
-            goalImg4.src = data.homepage.media.goal13;
-            goalImg4.setAttribute("class", "goalImg");
-            goalImg4.setAttribute("alt", "Image for goal 13 from the UN website");
+        const goalImg4 = document.createElement("img");
+        goalImg4.src = data.homepage.media.goal13;
+        goalImg4.setAttribute("class", "goalImg");
+        goalImg4.setAttribute("alt", "Image for goal 13 from the UN website");
 
-            goalImages.appendChild(goalsVideo);
-            goalImages.appendChild(goalImg2);
-            goalImages.appendChild(goalImg3);
-            goalImages.appendChild(goalImg4);
-        })
-        .catch(error => console.error("Error fetching JSON data:", error));
-    }
+        goalImages.appendChild(goalsVideo);
+        goalImages.appendChild(goalImg2);
+        goalImages.appendChild(goalImg3);
+        goalImages.appendChild(goalImg4);
+    })
+    .catch(error => console.error("Error fetching JSON data:", error));
 });
--- C3-Project/EJS/views/Scripts/signup.js
+++ C3-Project/EJS/views/Scripts/signup.js
@@ -1,127 +1,125 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (document.querySelector('#signup')){ //checks if the current page is the signup using an id="signup" in the opening html tag
-        fetch('../JSON/main.json')
-        .then(response => response.json())
-        .then(data => {
+    fetch('../JSON/main.json')
+    .then(response => response.json())
+    .then(data => {
 
-            /*Loading heading into central element*/
+        /*Loading heading into central element*/
 
-            const mainPara = document.getElementById("formPara");
+        const mainPara = document.getElementById("formPara");
 
-            const paraHeader = document.createElement("h2");
-            paraHeader.textContent = data.signup.content.title;
+        const paraHeader = document.createElement("h2");
+        paraHeader.textContent = data.signup.content.title;
 
-            /* Creating the form inside a section element (inside the central element) */
-            const form = document.createElement("form");
-            form.setAttribute("id", "myForm");
-            form.setAttribute("method", "POST");
+        /* Creating the form inside a section element (inside the central element) */
+        const form = document.createElement("form");
+        form.setAttribute("id", "myForm");
+        form.setAttribute("method", "POST");
 
-            const fieldset = document.createElement("fieldset");
+        const fieldset = document.createElement("fieldset");
 
-            // First Name
-            const fNameLabel = document.createElement("label");
-            fNameLabel.textContent = "First Name*:";
+        // First Name
+        const fNameLabel = document.createElement("label");
+        fNameLabel.textContent = "First Name*:";
 
-            const fNameInput = document.createElement("input");
-            fNameInput.setAttribute("id", "firstName");
-            fNameInput.setAttribute("type", "text");
-            fNameInput.setAttribute("required", true);
-            fNameInput.setAttribute("name", "firstName")
+        const fNameInput = document.createElement("input");
+        fNameInput.setAttribute("id", "firstName");
+        fNameInput.setAttribute("type", "text");
+        fNameInput.setAttribute("required", true);
+        fNameInput.setAttribute("name", "firstName")
 
-            // Last Name
-            const lNameLabel = document.createElement("label");
-            lNameLabel.textContent = "Surname*:";
+        // Last Name
+        const lNameLabel = document.createElement("label");
+        lNameLabel.textContent = "Surname*:";
 
-            const lNameInput = document.createElement("input");
-            lNameInput.setAttribute("id", "lastName");
-            lNameInput.setAttribute("type", "text");
-            lNameInput.setAttribute("required", true);
-            lNameInput.setAttribute("name", "lastName")
+        const lNameInput = document.createElement("input");
+        lNameInput.setAttribute("id", "lastName");
+        lNameInput.setAttribute("type", "text");
+        lNameInput.setAttribute("required", true);
+        lNameInput.setAttribute("name", "lastName")
 
-            // Email
-            const emailLabel = document.createElement("label");
-            emailLabel.textContent = "Email*:";
+        // Email
+        const emailLabel = document.createElement("label");
+        emailLabel.textContent = "Email*:";
 
-            const emailInput = document.createElement("input");
-            emailInput.setAttribute("id", "email");
-            emailInput.setAttribute("type", "email");
-            emailInput.setAttribute("required", true);
-            emailInput.setAttribute("name", "email")
+        const emailInput = document.createElement("input");
+        emailInput.setAttribute("id", "email");
+        emailInput.setAttribute("type", "email");
+        emailInput.setAttribute("required", true);
+        emailInput.setAttribute("name", "email")
 
-            // Comments
-            const commentsLabel = document.createElement("label");
-            commentsLabel.textContent = "Comments:";
+        // Comments
+        const commentsLabel = document.createElement("label");
+        commentsLabel.textContent = "Comments:";
 
-            const commentsTextarea = document.createElement("textarea");
-            commentsTextarea.setAttribute("id", "comments");
-            commentsTextarea.setAttribute("name", "comments");
+        const commentsTextarea = document.createElement("textarea");
+        commentsTextarea.setAttribute("id", "comments");
+        commentsTextarea.setAttribute("name", "comments");
 
-            const submitInput = document.createElement("input");
-            submitInput.setAttribute("type", "submit");
-            submitInput.setAttribute("value", "Send comments");
-            submitInput.setAttribute("id", "submitBtn");
+        const submitInput = document.createElement("input");
+        submitInput.setAttribute("type", "submit");
+        submitInput.setAttribute("value", "Send comments");
+        submitInput.setAttribute("id", "submitBtn");
 
-            const reqFieldPara = document.createElement("p");
-            reqFieldPara.setAttribute("id", "reqField");
-            reqFieldPara.textContent = "* = Required field";
+        const reqFieldPara = document.createElement("p");
+        reqFieldPara.setAttribute("id", "reqField");
+        reqFieldPara.textContent = "* = Required field";
 
-            fieldset.appendChild(fNameLabel);
-            fieldset.appendChild(fNameInput);
-            fieldset.appendChild(lNameLabel);
-            fieldset.appendChild(lNameInput);
-            fieldset.appendChild(emailLabel);
-            fieldset.appendChild(emailInput);
-            fieldset.appendChild(commentsLabel);
-            fieldset.appendChild(commentsTextarea);
-            fieldset.appendChild(submitInput);
-            fieldset.appendChild(reqFieldPara);
+        fieldset.appendChild(fNameLabel);
+        fieldset.appendChild(fNameInput);
+        fieldset.appendChild(lNameLabel);
+        fieldset.appendChild(lNameInput);
+        fieldset.appendChild(emailLabel);
+        fieldset.appendChild(emailInput);
+        fieldset.appendChild(commentsLabel);
+        fieldset.appendChild(commentsTextarea);
+        fieldset.appendChild(submitInput);
+        fieldset.appendChild(reqFieldPara);
 
-            form.appendChild(fieldset);
+        form.appendChild(fieldset);
 
-            mainPara.appendChild(paraHeader);
-            mainPara.appendChild(form);
+        mainPara.appendChild(paraHeader);
+        mainPara.appendChild(form);
 
 
-            // Sends data to server and receieves response
-            form.addEventListener('submit', (e)=>{
-                e.preventDefault();
-                const formBody = {
-                    fName:fNameInput.value,
-                    lName:lNameInput.value,
-                    email:emailInput.value,
-                    comments:commentsTextarea.value
+        // Sends data to server and receieves response
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            const formBody = {
+                fName:fNameInput.value,
+                lName:lNameInput.value,
+                email:emailInput.value,
+                comments:commentsTextarea.value
+            };
+            const requestHeaders = {
+                "Content-Type": "application/json"
+            };
+
+            let response;
+
+            fetch("/signup", {
+                method: "POST",
+                headers: requestHeaders,
+                body: JSON.stringify(formBody)
+            })
+            .then(response => response.json())
+            .then(responsedata => {
+                response = responsedata;
+            })
+            .then(() => {
+                for (const [key, value] of Object.entries(response)) {console.log(`${key}: ${value}`);};
+                if (response.send == true) {
+                    console.log("Sending email");
+                    window.alert(`You have successfully signed up for the newsletter! Check ${response.email} for confirmation.`);
+                }
+                else {
+                    console.log("Not sending email");
+                    window.alert(`One or more fields was not valid. Please try again.`);
                 };
-                const requestHeaders = {
-                    "Content-Type": "application/json"
-                };
-
-                let response;
-
-                fetch("/signup", {
-                    method: "POST",
-                    headers: requestHeaders,
-                    body: JSON.stringify(formBody)
-                })
-                .then(response => response.json())
-                .then(responsedata => {
-                    response = responsedata;
-                })
-                .then(() => {
-                    for (const [key, value] of Object.entries(response)) {console.log(`${key}: ${value}`);};
-                    if (response.send == true) {
-                        console.log("Sending email");
-                        window.alert(`You have successfully signed up for the newsletter! Check ${response.email} for confirmation.`);
-                    }
-                    else {
-                        console.log("Not sending email");
-                        window.alert(`One or more fields was not valid. Please try again.`);
-                    };
-                })
-                //.then(window.location.reload())
-                .catch(error => console.log(error));
-            });
-        })
-        .catch(error => console.error("Error fetching JSON data:", error));
-    }
+            })
+            //.then(window.location.reload())
+            .catch(error => console.log(error));
+        });
+    })
+    .catch(error => console.error("Error fetching JSON data:", error));
 });
